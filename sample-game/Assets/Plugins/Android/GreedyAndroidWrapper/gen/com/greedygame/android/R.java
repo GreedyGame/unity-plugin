4310c0ca25d6d818f1b2490d0ba6a68ef6cd7af5
Arink <arinkverma@gmail.com>
2016-05-01 02:20:09 +0530

    updated readme

--- sample-game/Assets/Plugins/Android/GreedyAndroidWrapper/gen/com/greedygame/android/R.java
+++ sample-game/Assets/Plugins/Android/GreedyAndroidWrapper/gen/com/greedygame/android/R.java
@@ -8,24 +8,32 @@
 package com.greedygame.android;
 
 public final class R {
+    public static final class anim {
+        public static int fade_in_greedy=0x7f040000;
+        public static int fade_out_greedy=0x7f040001;
+        public static int hold_greedy=0x7f040002;
+        public static int pull_left_greedy=0x7f040003;
+        public static int pull_right_greedy=0x7f040004;
+        public static int scale_down=0x7f040005;
+        public static int scale_up=0x7f040006;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static int greedy_close=0x7f020000;
-        public static int xgreedy_close=0x7f020001;
     }
     public static final class id {
-        public static int gg_head_bannerview=0x7f060003;
-        public static int gg_head_layout=0x7f060000;
-        public static int gg_head_outside=0x7f060001;
-        public static int gg_head_webview=0x7f060002;
-        public static int loadingPanel=0x7f060004;
+        public static int gg_head_bannerview=0x7f070003;
+        public static int gg_head_layout=0x7f070000;
+        public static int gg_head_outside=0x7f070001;
+        public static int gg_head_webview=0x7f070002;
+        public static int loading_panel=0x7f070004;
     }
     public static final class layout {
         public static int float_main=0x7f030000;
     }
     public static final class string {
-        public static int greedy_game_profile=0x7f040000;
+        public static int greedy_game_profile=0x7f050000;
     }
     public static final class style {
         /** 
@@ -38,11 +46,11 @@ public final class R {
 	        backward-compatibility can go here.
 	    
          */
-        public static int AppBaseTheme=0x7f050000;
+        public static int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f050002;
-        public static int Theme_Transparent=0x7f050001;
+        public static int AppTheme=0x7f060002;
+        public static int Theme_Transparent=0x7f060001;
     }
 }
