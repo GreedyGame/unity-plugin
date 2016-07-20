dd45ec7afe70721df8a7dbb87b41900fa6e2f75d
niks_nixac <nikhil@greedygame.com>
2016-07-20 16:23:16 +0530

    showWindow corrected

---
+++ sample-game/Assets/Scene/showScriptGG.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using GreedyGame.Runtime.Common;
+using GreedyGame.Platform;
+
+public class showScriptGG : MonoBehaviour {
+
+	public string FloatUnit = null;
+	private GreedyAdManager ggAdManager = null;
+
+	void Awake (){	
+		ggAdManager = GreedyAdManager.Instance;
+	}
+
+	public void showEngage() {
+		ggAdManager.showEngagementWindow (FloatUnit);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
